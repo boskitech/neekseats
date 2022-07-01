5f1889a9c271d8f83534c1f5c262d6cf9cb8efda
boski <boskitechh@gmail.com>
2022-07-01 21:46:48 +0100

    cart in progress

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,4 +1,20 @@
+import { styled } from "@mui/system";
+
 const Cart = () => {
+  const CartBody = styled("div")(({ theme }) => ({
+    width: "70%",
+    margin: "130px auto",
+    [theme.breakpoints.up("xl")]: {
+      width: "60%",
+    },
+    [theme.breakpoints.down("lg")]: {
+      width: "80%",
+    },
+    [theme.breakpoints.down("md")]: {
+      width: "90%",
+      margin: "40px auto",
+    },
+  }));
   return <div>Cart</div>;
 };
 
